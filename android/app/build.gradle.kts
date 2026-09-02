d4c090efd8666b1ccf93f728478f78a6ad027abf
Ali Gol Mohamadi <agrkmgmg@gmail.com>
2026-09-02 20:20:49 +0330

    Align Android Java and Kotlin targets to JDK 17

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -20,9 +20,18 @@ android {
         compose = true
     }
 
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
     packaging {
         resources.excludes += "/META-INF/{AL2.0,LGPL2.1}"
     }
+}
+
+kotlin {
+    jvmToolchain(17)
 }
 
 dependencies {
